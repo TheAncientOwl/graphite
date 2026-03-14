3e74080b66122d908c66ca49b95c6f8318bf93a1
TheAncientOwl <dele.alex.o.o@gmail.com>
2026-03-14 08:32:36 +0200

    move plugin related code to separate file

---
+++ src/main_plugins.cpp
@@ -0,0 +1,67 @@
+/// --------------------------------------------------------------------------
+///                     Copyright (c) by ImGui-Lab 2026
+/// --------------------------------------------------------------------------
+/// @license https://github.com/TheAncientOwl/imgui-lab/blob/main/LICENSE
+///
+/// @file main_plugins.cpp
+/// @author Alexandru Delegeanu
+/// @version 0.2
+/// @brief Plugins test entry point.
+///
+
+#include <memory>
+#include <stdlib.h>
+
+#include "imgui/imgui.h"
+
+#include "core/logger/Logger.hpp"
+#include "core/renderer/Renderer.hpp"
+
+#include "demo/BasicTableApp.hpp"
+
+#include "core/plugin/DynamicLibrary.hpp"
+#include "core/plugin/IPlugin.hpp"
+
+int main()
+{
+    LOG_SCOPE("");
+
+    // auto app = std::make_shared<Graphite::Demo::BasicTableApp>();
+
+    // auto renderer = Graphite::Core::Renderer::CreateRenderer();
+    // renderer->Init();
+    // renderer->Render(app);
+    // renderer->Cleanup();
+
+#ifdef _WIN32
+    auto const lib_path{std::filesystem::current_path() / "plugins" / "libdummy_plugin.dll"};
+#elif defined(__APPLE__)
+    auto const lib_path{std::filesystem::current_path() / "plugins" / "libdummy_plugin.dylib"};
+#else
+    auto const lib_path{std::filesystem::current_path() / "plugins" / "libdummy_plugin.so"};
+#endif
+
+    Graphite::Core::Plugin::DynamicLibrary dummyLib(lib_path);
+    if (dummyLib.isLoaded())
+    {
+        auto factory = (CreateGraphitePluginFactory)dummyLib.getSymbol("CreateGraphitePlugin");
+        if (factory)
+        {
+            auto plugin = std::unique_ptr<Graphite::Core::Plugin::IGraphitePlugin>(factory());
+            LOG_INFO("Successfully loaded {}!", plugin->GetName());
+            plugin->Execute();
+        }
+        else
+        {
+            LOG_CRITICAL("Failed to load symbol CreateGraphitePlugin at {}", lib_path.c_str());
+            return EXIT_FAILURE;
+        }
+    }
+    else
+    {
+        LOG_CRITICAL("Could not load dummy lib at {}", lib_path.c_str());
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
